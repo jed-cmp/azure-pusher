6e17af62630e00f06f25c371645a6805044edc3f
Joseph Duran <jed.cmp@gmail.com>
2020-09-24 00:20:55 -0500

    create initial pusher notifications app

---
+++ app/src/main/java/jed/cmp/azurepusher/MainActivity.kt
@@ -0,0 +1,15 @@
+package jed.cmp.azurepusher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pusher.pushnotifications.PushNotifications;
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        PushNotifications.start(applicationContext, "04374500-fb5e-41b6-9bbe-9acb3ee2e174");
+        PushNotifications.addDeviceInterest("hello");
+        setContentView(R.layout.activity_main)
+    }
+}
