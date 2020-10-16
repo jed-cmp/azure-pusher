50595acd9c20b6ae2f9caea19f0f57a93dcc26c2
Joseph Duran <jed.cmp@gmail.com>
2020-10-16 01:24:49 -0500

    update pusher interests

--- app/src/main/java/jed/cmp/azurepusher/MainActivity.kt
+++ app/src/main/java/jed/cmp/azurepusher/MainActivity.kt
@@ -2,14 +2,15 @@ package jed.cmp.azurepusher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.pusher.pushnotifications.PushNotifications;
+import com.pusher.pushnotifications.PushNotifications
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        PushNotifications.start(applicationContext, "04374500-fb5e-41b6-9bbe-9acb3ee2e174");
-        PushNotifications.addDeviceInterest("hello");
+        PushNotifications.start(applicationContext, "04374500-fb5e-41b6-9bbe-9acb3ee2e174")
+        PushNotifications.addDeviceInterest("ethereum-gas-prices")
+        PushNotifications.addDeviceInterest("alpaca-trading-bot")
         setContentView(R.layout.activity_main)
     }
 }
