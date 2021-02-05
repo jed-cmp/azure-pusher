ebdb915fa7b386c2ffe2233b85b40b00b2ce2710
Joseph Duran <jed.cmp@gmail.com>
2021-02-04 20:57:37 -0600

    remove warnings

--- app/src/main/java/jed/cmp/azurepusher/MainActivity.kt
+++ app/src/main/java/jed/cmp/azurepusher/MainActivity.kt
@@ -1,7 +1,6 @@
 package jed.cmp.azurepusher
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
